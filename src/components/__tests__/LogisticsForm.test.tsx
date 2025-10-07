452b0d74ac23c566de4f4572bc0c985cddcddbe7
Kyletho2020 <kyle.tho@icloud.com>
2025-10-07 09:54:38 -0700

    Copy src/components/__tests__/LogisticsForm.test.tsx from Bolt3.0

---
+++ src/components/__tests__/LogisticsForm.test.tsx
@@ -0,0 +1,51 @@
+import test from 'node:test';
+import assert from 'node:assert';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import LogisticsForm from '../LogisticsForm';
+
+test('LogisticsForm renders Logistics Quote heading', () => {
+  const data = {
+    pieces: [
+      {
+        id: 'piece-1',
+        description: '',
+        quantity: 1,
+        length: '',
+        width: '',
+        height: '',
+        weight: ''
+      }
+    ],
+    pickupAddress: '',
+    pickupCity: '',
+    pickupState: '',
+    pickupZip: '',
+    deliveryAddress: '',
+    deliveryCity: '',
+    deliveryState: '',
+    deliveryZip: '',
+    shipmentType: '',
+    truckType: '',
+    storageType: '',
+    storageSqFt: ''
+  };
+
+  const html = renderToString(
+    <LogisticsForm
+      data={data}
+      selectedPieces={[]}
+      onFieldChange={() => {}}
+      onPieceChange={() => {}}
+      addPiece={() => {}}
+      removePiece={() => {}}
+      togglePieceSelection={() => {}}
+      deleteSelectedPieces={() => {}}
+      movePiece={() => {}}
+      register={() => ({ onChange: () => {}, onBlur: () => {}, ref: () => {} }) as any}
+      errors={{}}
+    />
+  );
+
+  assert.ok(html.includes('Logistics Quote'));
+});
