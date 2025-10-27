74908c67d2fab4827053493a039328b6d849d200
Kyletho2020 <kyle.tho@icloud.com>
2025-10-27 10:05:24 -0700

    feat: integrate HubSpot Breeze Assistant widget

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id = "app">
+    <!-- Your existing content -->
+    <HubSpotBreeze />
+  
+  </div>
+</template>
+
+<script setup lang="ts">
+import HubSpotBreeze from './components/HubSpotBreeze.vue'
+
+defineComponents({
+  HubSpotBreeze
+})
+</script>
